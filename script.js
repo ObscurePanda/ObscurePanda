a9b99417e666bb8d54cc9669a9cabe922938dfc0
ObscurePanda <137990707+ObscurePanda@users.noreply.github.com>
2023-06-28 01:50:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-window.addEventListener("DOMContentLoaded", function() {
-  const tabs = document.querySelectorAll(".tabs input[type='radio']");
-  
-  tabs.forEach(tab => {
-    tab.addEventListener("change", function() {
-      const tabContentId = this.nextElementSibling.getAttribute("id");
-      const tabContents = document.querySelectorAll(".tab-content");
-      
-      tabContents.forEach(content => {
-        if (content.getAttribute("id") === tabContentId) {
-          content.style.display = "block";
-        } else {
-          content.style.display = "none";
-        }
-      });
-    });
+const tabs = document.querySelectorAll('input[name="tab"]');
+const tabContents = document.querySelectorAll('.tab-content');
+
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener('change', () => {
+    // Hide all tab contents
+    for (let j = 0; j < tabContents.length; j++) {
+      tabContents[j].classList.remove('show');
+    }
+
+    // Show the selected tab content
+    tabContents[i].classList.add('show');
   });
-});
+}

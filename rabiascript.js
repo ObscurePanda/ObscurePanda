324e19badfde1cddc537a09eee76e4be46d1029a
ObscurePanda <137990707+ObscurePanda@users.noreply.github.com>
2023-06-29 00:50:20 -0700

    Update rabiascript.js

--- rabiascript.js
+++ rabiascript.js
@@ -1,64 +1,84 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    const characterSheet = document.getElementById('characterSheet');
+  const characterSheet = document.getElementById('characterSheet');
 
-    // Generate character sheet content
-    const characterAttributes = {
-        'Strength': 'XXXXXXOO',
-        'Charisma': 'XXXXOOOO',
-        'Perception': 'XXXXXOOO',
-        // ... Add other attributes
-    };
+  // Generate character sheet content
+  const characterAttributes = {
+    'Strength': 'XXXXXXOO',
+    'Charisma': 'XXXXOOOO',
+    'Perception': 'XXXXXOOO',
+    // ... Add other attributes
+  };
 
-    const characterSheetSections = [
-        {
-            title: 'Attributes',
-            content: characterAttributes
-        },
-        // ... Add other sections with their content
-    ];
+  const characterSheetSections = [
+    {
+      title: 'Attributes',
+      content: characterAttributes
+    },
+    // ... Add other sections with their content
+  ];
 
-    // Populate character sheet
-    characterSheetSections.forEach((section) => {
-        const sectionContainer = document.createElement('div');
-        sectionContainer.classList.add('characterSheetSection');
+  // Populate character sheet
+  characterSheetSections.forEach((section) => {
+    const sectionContainer = document.createElement('div');
+    sectionContainer.classList.add('characterSheetSection');
 
-        const sectionTitle = document.createElement('div');
-        sectionTitle.classList.add('characterSheetSectionTitle');
-        sectionTitle.textContent = section.title;
+    const sectionTitle = document.createElement('div');
+    sectionTitle.classList.add('characterSheetSectionTitle');
+    sectionTitle.textContent = section.title;
 
-        sectionContainer.appendChild(sectionTitle);
+    sectionContainer.appendChild(sectionTitle);
 
-        if (section.subtitle) {
-            const sectionSubtitle = document.createElement('div');
-            sectionSubtitle.classList.add('characterSheetSectionSubtitle');
-            sectionSubtitle.textContent = section.subtitle;
+    const sectionContent = document.createElement('div');
+    sectionContent.classList.add('characterSheetSectionContent');
 
-            sectionContainer.appendChild(sectionSubtitle);
-        }
+    Object.entries(section.content).forEach(([attribute, rating]) => {
+      const attributeContainer = document.createElement('div');
+      attributeContainer.classList.add('characterSheetSectionContentItem');
 
-        const sectionContent = document.createElement('div');
-        sectionContent.classList.add('characterSheetSectionContent');
+      const attributeTitle = document.createElement('span');
+      attributeTitle.classList.add('characterAttribute', 'characterAttributeTitle');
+      attributeTitle.textContent = attribute + ': ';
 
-        Object.entries(section.content).forEach(([attribute, rating]) => {
-            const attributeContainer = document.createElement('div');
-            attributeContainer.classList.add('characterSheetSectionContentItem');
+      const attributeRating = document.createElement('span');
+      attributeRating.classList.add('characterAttribute', 'characterAttributeRatings');
+      attributeRating.textContent = rating;
 
-            const attributeTitle = document.createElement('span');
-            attributeTitle.classList.add('characterAttribute', 'characterAttributeTitle');
-            attributeTitle.textContent = attribute + ': ';
+      attributeContainer.appendChild(attributeTitle);
+      attributeContainer.appendChild(attributeRating);
 
-            const attributeRating = document.createElement('span');
-            attributeRating.classList.add('characterAttribute', 'characterAttributeRatings');
-            attributeRating.textContent = rating;
-
-            attributeContainer.appendChild(attributeTitle);
-            attributeContainer.appendChild(attributeRating);
-
-            sectionContent.appendChild(attributeContainer);
-        });
-
-        sectionContainer.appendChild(sectionContent);
-
-        characterSheet.appendChild(sectionContainer);
+      sectionContent.appendChild(attributeContainer);
     });
+
+    sectionContainer.appendChild(sectionContent);
+
+    characterSheet.appendChild(sectionContainer);
+  });
+
+  // Tab functionality
+  function openTab(tabName) {
+    var tabs = document.getElementsByClassName('tab');
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].style.display = 'none';
+    }
+
+    var tabButtons = document.getElementsByClassName('tab-button');
+    for (var i = 0; i < tabButtons.length; i++) {
+      tabButtons[i].classList.remove('active');
+    }
+
+    document.getElementById(tabName).style.display = 'block';
+    document.getElementById(tabName + '-button').classList.add('active');
+  }
+
+  // Set the initial active tab
+  openTab('personality');
+
+  // Add event listeners to tab buttons
+  const tabButtons = document.getElementsByClassName('tab-button');
+  for (let i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].addEventListener('click', function () {
+      const tabName = this.getAttribute('data-tab');
+      openTab(tabName);
+    });
+  }
 });
